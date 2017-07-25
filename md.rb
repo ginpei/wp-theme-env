ae18f585d94defac4febcec27018abb2fbb0410b
ginpei <ginpei@gmail.com>
2017-07-25 10:03:13 -0700

    read stdin

--- md.rb
+++ md.rb
@@ -1,5 +1,3 @@
 require 'github/markup'
 
-s = GitHub::Markup.render('README.markdown', "* One\n* Two")
-# s = GitHub::Markup.render_s(GitHub::Markups::MARKUP_MARKDOWN, "* One\n* Two")
-p s
+puts GitHub::Markup.render_s(GitHub::Markups::MARKUP_MARKDOWN, STDIN.read)
