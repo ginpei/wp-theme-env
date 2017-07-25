763273769872887869e8d50c66424280b10bebe7
ginpei <ginpei@gmail.com>
2017-07-25 10:48:25 -0700

    make it secure using temporary files

--- md.php
+++ md.php
@@ -1,7 +1,16 @@
 <?php
 function md($source) {
-	exec('echo "' . $source . '" | ruby md.rb', $lines);  // TODO make it secure
+	$tmpfname = tempnam("/tmp", "md");
+
+	$handle = fopen($tmpfname, "w");
+	fwrite($handle, $source);
+	fclose($handle);
+
+	exec("cat $tmpfname | ruby md.rb", $lines);  // TODO make it secure
 	$result = join("\n", $lines);
+
+	unlink($tmpfname);
+
 	return $result;
 }
 
